ce7320e281c34e17843afe1565c376385f56f257
Mark <kuzmitsch33@gmail.com>
2026-02-15 00:03:14 +0100

    remove old code

--- examples/simple_download.rs
+++
@@ -1,7 +0,0 @@
-use dust::download::Task;
-
-#[tokio::main]
-async fn main() {
-    let task = Task::try_from("http://212.183.159.230/100MB.zip").unwrap();
-    task.download().await.unwrap();
-}
--- tests/read_config_test.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-mod tests {
-    use std::fs;
-
-    #[tokio::test]
-    async fn read_config_test() {
-        assert!(fs::read_to_string("config.json").is_ok());
-    }
-}
