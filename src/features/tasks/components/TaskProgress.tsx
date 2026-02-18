a95294b91105293fe348a42f5ecd46ba72ec4d9f
mark-rez <markreznikov@mailbox.org>
2026-02-18 22:23:19 +0100

    change progress bar to fast-slow-fast easing

--- src/features/tasks/components/TaskProgress.tsx
+++ src/features/tasks/components/TaskProgress.tsx
@@ -1,19 +1,36 @@
+const OPACITY_TRANSITION_PERCENTAGE = 14;
+
 interface TaskProgress {
   progress: number;
 }
 
 export default function TaskProgress({ progress }: TaskProgress) {
-  const percentage = Math.round(progress * 100);
+  const easedProgress = fastSlowFast(progress);
+  const percentage = Math.round(easedProgress * 100);
 
   return (
     <div className="relative w-[85%] h-5 rounded-full bg-accent/5 outline outline-secondary-text/20 outline-offset-2">
       <div
-        className="absolute top-0 left-0 h-full rounded-full bg-linear-to-r from-accent/50 to-accent transition-all duration-300 ease-in-out"
+        className="absolute top-0 left-0 h-full rounded-full bg-linear-to-r from-accent/50 to-accent transition-all duration-200 ease-in-out"
         style={{ width: `${percentage}%` }}
-      />
-      <div className="absolute inset-0 flex items-center justify-center text-sm pointer-events-none">
-        {percentage}
+      >
+        <div
+          className={`absolute right-2 flex items-center justify-center text-sm pointer-events-none transition-colors  ${percentage < OPACITY_TRANSITION_PERCENTAGE ? "text-transparent" : "text-primary-background"}`}
+        >
+          {percentage}
+        </div>
       </div>
     </div>
+  );
+}
+
+// between -0.8 and 0, where -0.8 is the fastest
+const deviation = -0.5;
+
+function fastSlowFast(t: number) {
+  return (
+    (1 - 2 * deviation) * t +
+    deviation +
+    deviation * Math.sin(Math.PI * (t - 0.5))
   );
 }
